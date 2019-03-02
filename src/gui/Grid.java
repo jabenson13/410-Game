8fd47589372155ae600ca354da0ec29015f3b656
Luke Vacura <lvacura14@winona.edu>
2019-03-01 20:17:37 -0600

    Update Grid.java

--- src/gui/Grid.java
+++ src/gui/Grid.java
@@ -4,7 +4,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -12,67 +16,80 @@ import javax.swing.border.LineBorder;
 
 public class Grid extends JPanel{
 	
+	static Image defaultTile; static Image greenTile; static Image redTile;
+	
 	int columns;
 	int rows;
-	static JTextField[][] cells = new JTextField[5][5];
+	static BackgroundPanel[][] cells = new BackgroundPanel[5][5];
 	static final int bord = 1;
 	static LineBorder Border = new LineBorder(Color.BLACK,bord);
 	Font f = new Font("Sans Serif", Font.BOLD, 100);
 	static Font g = new Font("Times New Roman", Font.BOLD, 125);
 	
-	public Grid(int col, int row) {		
+	public Grid(int col, int row) throws IOException {		
+		defaultTile = ImageIO.read(new File("Assets/DarkTile.png"));
+		greenTile = ImageIO.read(new File("Assets/DarkTileGreenDot.png"));
+		redTile = ImageIO.read(new File("Assets/DarkTileRedDot.png"));
+		
 		columns = col;
 		rows = row;
 		setLayout(new GridLayout(columns, rows));
 				
 		for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                cells[i][j] = new JTextField() {
+                cells[i][j] = new BackgroundPanel(redTile) {
                     @Override
                     public Dimension getPreferredSize() {
                         return new Dimension(20, 20);
                     }
                 };
-                cells[i][j].setEditable(false);
+                //cells[i][j].setEditable(false);
                 
                 if(i == 0 && j == 0) {
-                	cells[i][j].setText("");
+                	cells[i][j].setImage(defaultTile);
                 }
                 else if(i == 0) {
                 	String playerName = "Player " + j;
-                	cells[i][j].setText(playerName);
+                	//cells[i][j].setText(playerName);
+                	cells[i][j].setImage(defaultTile);
                 }
                 else if(j == 0) {
                 	String playerName = "Player " + i;
-                	cells[i][j].setText(playerName);
+                	//cells[i][j].setText(playerName);
+                	cells[i][j].setImage(defaultTile);
                 }
                 else if(i == 1 && j == 1) {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
+                	cells[i][j].setImage(greenTile);
                     cells[i][j].setFont(g);
                     cells[i][j].setForeground(Color.GREEN);
                 }
                 else if(i == 2 && j == 2) {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
+                	cells[i][j].setImage(greenTile);
                     cells[i][j].setFont(g);
                     cells[i][j].setForeground(Color.GREEN);
                 }
                 else if(i == 3 && j == 3) {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
+                	cells[i][j].setImage(greenTile);
                     cells[i][j].setFont(g);
                     cells[i][j].setForeground(Color.GREEN);
                 }
                 else if(i == 4 && j == 4) {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
+                	cells[i][j].setImage(greenTile);
                     cells[i][j].setFont(g);
                     cells[i][j].setForeground(Color.GREEN);
                 }
                 else {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
+                	cells[i][j].setImage(redTile);
                 	cells[i][j].setFont(g);
                 	cells[i][j].setForeground(Color.RED);
                 }
                 cells[i][j].setPreferredSize(new Dimension(50, 50));
-                cells[i][j].setHorizontalAlignment(JTextField.CENTER);
+                //cells[i][j].setHorizontalAlignment(JTextField.CENTER);
                 cells[i][j].setFocusTraversalKeysEnabled(false);
                 cells[i][j].setBorder(Border);
                 add(cells[i][j]);
@@ -116,40 +133,48 @@ public class Grid extends JPanel{
 //                cells[i][j].setEditable(false);
 				
 				if(i == 0 && j == 0) {
-                	cells[i][j].setText("");
+                	//cells[i][j].setText("");
+					cells[i][j].setImage(defaultTile);
                 }
                 else if(i == 0) {
                 	String playerName = "Player. " + j;
-                	cells[i][j].setText(playerName);
+                	//cells[i][j].setText(playerName);
+                	cells[i][j].setImage(defaultTile);
                 }
                 else if(j == 0) {
                 	String playerName = "Player " + i;
-                	cells[i][j].setText(playerName);
+                	//cells[i][j].setText(playerName);
+                	cells[i][j].setImage(defaultTile);
                 }
                 else if(i == 1 && j == 1) {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
                     cells[i][j].setFont(g);
                     cells[i][j].setForeground(Color.GREEN);
+                    cells[i][j].setImage(greenTile);
                 }
                 else if(i == 2 && j == 2) {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
                     cells[i][j].setFont(g);
                     cells[i][j].setForeground(Color.GREEN);
+                    cells[i][j].setImage(greenTile);
                 }
                 else if(i == 3 && j == 3) {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
                     cells[i][j].setFont(g);
                     cells[i][j].setForeground(Color.GREEN);
+                    cells[i][j].setImage(greenTile);
                 }
                 else if(i == 4 && j == 4) {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
                     cells[i][j].setFont(g);
                     cells[i][j].setForeground(Color.GREEN);
+                    cells[i][j].setImage(greenTile);
                 }
                 else {
-                	cells[i][j].setText(".");
+                	//cells[i][j].setText(".");
                 	cells[i][j].setFont(g);
                 	cells[i][j].setForeground(Color.RED);
+                	cells[i][j].setImage(redTile);
                 }
 			}
 		}
