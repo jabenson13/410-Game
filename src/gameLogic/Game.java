218425d9fd029d71193ee86b1def70a74c49da7f
Luke Vacura <lvacura14@winona.edu>
2019-03-03 16:35:22 -0600

    Game class - documentation and cleanup

--- src/gameLogic/Game.java
+++ src/gameLogic/Game.java
@@ -15,6 +15,7 @@ public class Game {
 	static ArrayList<Round> rounds;
 	static ArrayList<Integer> points = new ArrayList<Integer>();
 	static int roundCount = 0;
+	static int maxRounds = 10;
 	Listener list;
 	static ArrayList<GUI> guis = new ArrayList<GUI>();
 	
@@ -29,67 +30,83 @@ public class Game {
 		players = new ArrayList<Player>();
 		list = new Listener();
 		
+		// Adds Player 1 - ID, Name, Pts, Listener, GUI list
 		p1 = new Player(1, null, 0, list, guis); // the name would be given from the initial start by the GUI - done later
 		players.add(p1);
 		guis.add(new GUI("Player " + 1));
-		
 		guis.get(0).setListener(list);
 		list.setGUI(guis.get(0));
 		
-		System.out.println(guis.size());
+		// Adds Player 2
 		p2 = new Player(2, null, 0, list, guis);
 		players.add(p2);
 		guis.add(new GUI("Player " + 2));
-		
 		guis.get(1).setListener(list);
 		list.setGUI(guis.get(1));
+		
+		// Adds Player 3
 		p3 = new Player(3, null, 0, list, guis);
 		players.add(p3);
 		guis.add(new GUI("Player " + 3));
-		
 		guis.get(2).setListener(list);
 		list.setGUI(guis.get(2));
 		
-		System.out.println(guis.size());
+		// Adds Player 4
 		p4 = new Player(4, null, 0, list, guis);
 		players.add(p4);
 		guis.add(new GUI("Player " + 4));
-		
 		guis.get(3).setListener(list);
 		list.setGUI(guis.get(3));
 		
-		System.out.println(guis.size());
-		
-		System.out.println(players.size());
+		// Implements ArrayList for the points
 		for(int i=0; i<players.size(); i++) {
-			points.add(0);
+			points.add(players.get(i).points);
 		}
 		
+		// calls method newRound
 		newRound();
 	}
+	
+	// Begins a new round of the game
 	public static void newRound () {
 		roundCount++;
 		Interface.updatePoints(points);
 		Round round = new Round(roundCount, players);
-		//rounds.add(round);
 	}
+	
+	// Returns to the ArrayList of players
 	public static ArrayList<Player> getPlayers(){
 		return players;
 	}
-	public static boolean isGameDone() {
-		if(roundCount == 10) {
+	
+	// Simple boolean check to see if the game is done
+	public static boolean isGameDone() { 
+		if(roundCount == maxRounds) { 
+			// if incrementing round count = max rounds (10) .. return true
 			return true;
 		}
-		else {
+		else { 
+			// else return false
 			return false;
 		}
 	}
+	
+	// Sets the points in the player information through the ArrayList pts
 	public static void setPoints(ArrayList<Integer> pts) {
-		points = pts;
+		for(int i=0; i<players.size(); i++) {
+			players.get(i).points = pts.get(i);
+		}
 	}
+	
+	// Returns points based on the points in player info
 	public static ArrayList<Integer> getPoints(){
+		for(int i=0; i<players.size(); i++) {
+			points.set(i, players.get(i).points);
+		}
 		return points;
 	}
+	
+	// Returns the GUIs
 	public static ArrayList<GUI> getGUIs(){
 		return guis;
 	}
