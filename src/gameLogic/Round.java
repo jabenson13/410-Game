6dba980c87144f2f1e6284a7ae714a1fa677baeb
Luke Vacura <lvacura14@winona.edu>
2019-03-03 16:41:26 -0600

    Round updating

--- src/gameLogic/Round.java
+++ src/gameLogic/Round.java
@@ -5,68 +5,56 @@ import gui.Interface;
 
 public class Round {
 	int gameID;
-	int playerCount;
-	static int roundCount;
-	static ArrayList<Boolean> flags;
-	ArrayList<Player> players;
-	int initialDrawCount = 4;
-	int turnCount = 0;
-	String test;
-	ArrayList<Turn> turns;
+	static int roundCount;				// tracks the round count
+	static ArrayList<Boolean> flags;	// arraylist of boolean flags - true indicating a green dot, false for a red dot
+	ArrayList<Player> players;			// arraylist of players
+	int initialDrawCount = 18;			// how many numbers a player draws at the beginning of the round
+	int turnCount;						// tracks the turn count
 	
-	// constructor
+	// constructor for Round
 	public Round(int rNumber, ArrayList<Player> p) {
-		playerCount = p.size();
-		roundCount = rNumber;
-		Interface.setGameCount(roundCount);
-		Interface.resetWestPanel();
+		roundCount = rNumber; // the game tracks the round count and gives it to the round
 		players = p;
 		
-		flags = FlagHandler.flagReset(roundCount);
-		turns = new ArrayList<Turn>();
-		turnCount = 0;
-		Turn.turncount = 0;
-		Interface.setTurnCount(turnCount);
+		Interface.setGameCount(roundCount); // tells the GUI the round count so it can be displayed
+		Interface.resetWestPanel(); // resets the west panel (the player flags panel)
 		
-		for(int i=0; i<playerCount; i++) {
+		flags = FlagHandler.flagReset(roundCount); // resets the flags for the new round
+		Turn.turncount = 0; // resets the turn count for the new round
+		turnCount = Turn.turncount;
+		Interface.setTurnCount(turnCount); // tells the GUI the new turncount (reset)
+		
+		// resets the numbers for each of the players
+		for(int i=0; i<players.size(); i++) {
 			players.get(i).resetNumberPile();
 		}
 		
-		// organizes players according to draw order
-//		if(roundCount == 1) {
-//			currentOrder = players;
-//		}
-//		else {
-//			currentOrder = newOrder(currentOrder);
-//		}
-		
-		Turn turn = new Turn(players, flags);
-		
+		// each player draws 4 new numbers for the new round (their starting numbers)
 		for(int index=0; index<initialDrawCount; index++) {
-			for(int i=0; i<playerCount; i++) {
+			for(int i=0; i<players.size(); i++) {
 				players.get(i).drawNumber();
-				test = players.get(i).getHandtoString();
-				System.out.println(test);
-				
 			}
-			
 		}
+		
+		// begins a new turn
+		Turn turn = new Turn(players, flags);
 	}
 	
+	// A simple toString method
 	public String toString() {
-		String g = "";
-		
+		String g = "";	
 		for(int i = 0; i < flags.size(); i++) {
 			g += flags.get(i).toString() + " - ";
-		}
-		
-		
+		}	
 		return g;
 	}
+	
+	// getter for the flags
 	public static ArrayList<Boolean> getFlags(){
 		return flags;
 	}
 	
+	// Re-arranges the order between turns - WIP
 	public static ArrayList<Player> newOrder(ArrayList<Player> currentOrder){
 		ArrayList<Player> newOrder = currentOrder;
 		
